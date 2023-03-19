ebaa4116f185c5558b5c6b6a1b7cb6114921bd43
Joao <joao.airesmatos@gmail.com>
2023-03-19 13:11:19 +0000

    Changed project

---
+++ pages/api/llm/gpt3/mediumToThread.ts
@@ -0,0 +1,57 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import {
+    Configuration,
+    OpenAIApi,
+} from "openai";
+
+const OpenAI = require("openai-api");
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    try {
+        const mediumText = (<string>req.query.mediumText);
+        let basePromptPrefix = "";
+
+        console.log("mediumText");
+        basePromptPrefix = `
+Create me a Twitter thread from this Medium article:\n
+${mediumText}\n
+Twitter Thread:\n`;
+
+
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: basePromptPrefix }],
+            temperature: 0.7,
+        });
+
+        return res.status(200).json({
+            name: "",
+            content: completion.data.choices[0].message?.content,
+            success: true,
+        } as { name: string; content: string; format: string; success: boolean });
+    } catch (e: any) {
+        console.log(e);
+        let message =
+            e.response.data.message !== undefined ? e.response.data.message : "";
+        return res.status(400).json({
+            name: "",
+            reason: e.response !== undefined ? message : "",
+            success: false,
+        } as {
+            name: string;
+            reason: string;
+            success: boolean;
+        });
+    }
+}
---
+++ pages/api/webScrapping/getMediumSection.ts
@@ -0,0 +1,46 @@
+const cheerio = require('cheerio')
+const axios = require('axios')
+const Cors = require('cors')
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+    methods: ['POST', 'GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+    req: NextApiRequest,
+    res: NextApiResponse,
+    fn: Function
+) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result: any) => {
+            if (result instanceof Error) {
+                return reject(result)
+            }
+
+            return resolve(result)
+        })
+    })
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+
+    // Run the middleware
+    await runMiddleware(req, res, cors)
+    const url = (<string>req.query.url);
+    const { data } = await axios.get(url)
+    const $ = cheerio.load(data)
+    const section = $('section').text()
+
+    // Rest of the API logic
+    res.json({ section: section })
+
+
+}
