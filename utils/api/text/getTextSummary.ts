4620b0340ac720b92dcd22a48c49687259398070
Jota <joao.airesmatos@gmail.com>
2023-04-12 18:21:37 +0100

    Changed endpoint for summary

--- utils/api/text/getTextSummary.ts
+++ utils/api/text/getTextSummary.ts
@@ -1,19 +1,30 @@
 async function getTextSummary(data: any, url: string) {
-    return await fetch('/api/llm/gpt3/textToSummary', {
+    return await fetch('https://langchain-py.vercel.app/', {
+        // return await fetch('/api/llm/gpt3/textToSummary', {
+        headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
         method: 'POST',
         body: JSON.stringify({
-            text: data,
+            docsT: data,
             url: url
         })
     })
         .then((res) => res.json())
         .then((data) => {
-            if (data.success === false) {
+            if (!data) {
                 return { content: "Error", success: false };
             }
             else {
-                return { content: data.content, success: true };
+                return { content: data.response, success: true };
             }
+            // if (data.success === false) {
+            //     return { content: "Error", success: false };
+            // }
+            // else {
+            //     return { content: data.content, success: true };
+            // }
         }).catch((err) => {
             return { content: "Error", success: false };
         });
