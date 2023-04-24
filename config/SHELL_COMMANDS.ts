f36bbbef14a5d82b92b43e2a6bac078816f84258
Jota <joao.airesmatos@gmail.com>
2023-04-24 11:50:35 +0100

    Fix

--- config/SHELL_COMMANDS.ts
+++
@@ -1,7 +0,0 @@
-export const SHELL_COMMANDS = {
-  installRequirements: 'pip install -r requirements.txt',
-  startAutoGPT: 'python scripts/main.py',
-  testLsLa: 'ls -la',
-  testMockSpinner: `bash ../scripts/mock-spinner.sh`,
-  testMockUserInput: `bash ../scripts/mock-user-input.sh`,
-} satisfies Record<string, string>;
